cfd9a75fb853e86a10914894ac6c01d1a1f1a513
Phan Xuan Giang <giang.phan@elca.vn>
2023-06-01 16:43:58 +0700

    change api action

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,6 @@ import { PipeLineRoute } from './pipeline.route'
 const router = express.Router()
 
 router.get('/status', (_req, res) => res.status(200).json({ status: 'OK' }))
-router.post('/pipeline', PipeLineRoute)
+router.use('/pipeline', PipeLineRoute)
 
 export const apiV1 = router
