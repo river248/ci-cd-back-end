a35643a393c8c850b24bab28396563bd5122c830
Phan Xuan Giang <giang.phan@elca.vn>
2023-06-02 00:17:48 +0700

    trigger pipeline

--- src/controllers/pipeline.controller.js
+++ src/controllers/pipeline.controller.js
@@ -1,6 +1,17 @@
 import { PipeLineService } from '~/services/pipeline.service'
 import { HttpStatusCode } from '~/utils/constants'
 
+const triggerWorkflow = async (req, res) => {
+    try {
+        const result = await PipeLineService.triggerWorkflow()
+        res.status(HttpStatusCode.OK).json(result)
+    } catch (error) {
+        res.status(HttpStatusCode.INTERNAL_SERVER).json({
+            message: error.message,
+        })
+    }
+}
+
 const handleDataFromGithubAction = (req, res) => {
     const result = PipeLineService.handleDataFromGithubActions(req.body)
 
@@ -9,5 +20,6 @@ const handleDataFromGithubAction = (req, res) => {
 }
 
 export const PipeLineController = {
+    triggerWorkflow,
     handleDataFromGithubAction,
 }
--- src/services/pipeline.service.js
+++ src/services/pipeline.service.js
@@ -1,3 +1,32 @@
+import { Octokit } from 'octokit'
+
+const triggerWorkflow = async () => {
+    const octokit = new Octokit({
+        auth: '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',
+    })
+
+    try {
+        await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+            owner: 'river248',
+            repo: 'ci-cd-back-end',
+            workflow_id: 'Github River Pipeline',
+            ref: 'master',
+            inputs: {
+                name: 'Mona the Octocat',
+                home: 'San Francisco, CA',
+            },
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+            },
+        })
+
+        return undefined
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+
 const handleDataFromGithubActions = (payload) => {
     const { workflow_job } = payload
     const { status, conclusion, head_branch, created_at, completed_at } = workflow_job
@@ -13,5 +42,6 @@ const handleDataFromGithubActions = (payload) => {
 }
 
 export const PipeLineService = {
+    triggerWorkflow,
     handleDataFromGithubActions,
 }
