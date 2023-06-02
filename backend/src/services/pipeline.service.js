25705f48f0af7563d50aec6dbc290f367a625a21
Phan Xuan Giang <giang.phan@elca.vn>
2023-06-03 02:09:26 +0700

    create metric

--- backend/src/services/pipeline.service.js
+++ backend/src/services/pipeline.service.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { env } from '~/configs/environment'
 import InternalServer from '~/errors/internalServer.error'
 import NotFound from '~/errors/notfound.error'
@@ -51,6 +52,32 @@ const summaryJob = (jobs) => {
     return { progress, successJob, totalJob }
 }
 
+const createMetric = async (jobs, metricName) => {
+    try {
+        const metricData = jobs.find((job) => job.name === metricName)
+        const { status } = metricData
+        let metric = {
+            name: metricName,
+            actual: 0,
+            total: 0,
+        }
+
+        if (status === workflowStatus.COMPLETED) {
+            const res = await axios.get(
+                'https://sonarcloud.io/api/qualitygates/project_status?projectKey=river248_ci-cd-github-actions',
+            )
+            const qualityGates = res.data?.projectStatus.conditions
+            const sonarOkQualityGates = qualityGates.filter((qualityGate) => qualityGate.status === 'OK').length
+            const sonarErrorQualityGates = qualityGates.filter((qualityGate) => qualityGate.status === 'ERROR').length
+            metric = { ...metric, actual: sonarOkQualityGates, total: sonarErrorQualityGates + sonarOkQualityGates }
+        }
+
+        return metric
+    } catch (error) {
+        throw new InternalServer(error.message)
+    }
+}
+
 //========================================================================================+
 //                                    PUBLIC FUNCTIONS                                    |
 //========================================================================================+
@@ -76,25 +103,31 @@ const triggerWorkflow = async (repo, branchName) => {
     }
 }
 
-const summary = (payload) => {
-    const { workflow_job, repository } = payload
-    const { status, conclusion, head_branch, head_sha, created_at, completed_at, name, steps } = workflow_job
-    const { progress, successJob, totalJob } = summaryJob(steps)
+const summary = async (payload) => {
+    try {
+        const { workflow_job, repository } = payload
+        const { status, conclusion, head_branch, head_sha, created_at, completed_at, name, steps } = workflow_job
+        const { progress, successJob, totalJob } = summaryJob(steps)
+        const codeQuality = await createMetric(steps, 'Code Quality')
 
-    const pipelineData = {
-        status: conclusion || status,
-        branchName: head_branch,
-        startDate: created_at,
-        endDate: completed_at,
-        commitId: head_sha,
-        repository: repository.name,
-        stage: name,
-        progress,
-        successJob,
-        totalJob,
+        const pipelineData = {
+            status: conclusion || status,
+            branchName: head_branch,
+            startDate: created_at,
+            endDate: completed_at,
+            commitId: head_sha,
+            repository: repository.name,
+            stage: name,
+            progress,
+            successJob,
+            totalJob,
+            metrics: [codeQuality],
+        }
+
+        return pipelineData
+    } catch (error) {
+        throw new InternalServer(error.message)
     }
-
-    return pipelineData
 }
 
 //========================================================================================+
