d50f76490a8ca92da6c9bd0cc4074bf8177e1de9
Phan Xuan Giang <giang.phan@elca.vn>
2023-06-24 13:53:33 +0700

    test report unit tests

--- backend/src/__tests__/sum.test.js
+++ backend/src/__tests__/sum.test.js
@@ -2,8 +2,14 @@ const testFunction = (a, b) => {
     return a + b
 }
 
-describe('sum module', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(testFunction(1, 2)).toBe(3)
-    })
+test('adds 1 + 2 to equal 3', () => {
+    expect(testFunction(1, 2)).toBe(3)
+})
+
+test('adds 1 + 6 to equal 7', () => {
+    expect(testFunction(1, 6)).toBe(3)
+})
+
+test('adds 1 + 8 to equal 9', () => {
+    expect(testFunction(1, 8)).toBe(3)
 })
