755c6c803b9702e33e3c5f81a18e5245a99ecc9c
Phan Xuan Giang <giang.phan@elca.vn>
2023-08-05 13:06:33 +0700

    find executions by date

---
+++ backend/src/validations/stage.validation.js
@@ -0,0 +1,31 @@
+import Joi from 'joi'
+
+import { HttpStatusCode } from '~/utils/constants'
+
+const findExecutionsByDate = async (req, res, next) => {
+    const condition = Joi.object({
+        startDateTime: Joi.string().trim().required().length(10),
+        endDateTime: Joi.string().trim().required().length(10),
+    })
+
+    try {
+        const { startDateTime, endDateTime } = await condition.validateAsync(req.query, { abortEarly: false })
+        const fromDate = new Date(startDateTime)
+        const toDate = new Date(endDateTime)
+
+        fromDate.toISOString()
+        toDate.toISOString()
+
+        if (fromDate > toDate) {
+            throw new Error('startDateTime must be less than or equal endDateTime !')
+        }
+
+        next()
+    } catch (error) {
+        res.status(HttpStatusCode.BAD_REQUEST).json({
+            error: error.message,
+        })
+    }
+}
+
+export const StageValidation = { findExecutionsByDate }
