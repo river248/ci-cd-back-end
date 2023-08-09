ac3cc648433535d817d4cf3586bca28214aaa0a9
Phan Xuan Giang <giang.phan@elca.vn>
2023-08-09 11:11:40 +0700

    styles for label

--- dashboard/src/components/ExecutionFilter/ExecutionFilter.js
+++ dashboard/src/components/ExecutionFilter/ExecutionFilter.js
@@ -4,16 +4,21 @@ import Stack from '@mui/material/Stack'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Typography from '@mui/material/Typography'
+import { useTheme } from '@mui/material/styles'
 
 import DateTimePicker from '~/components/DateTimePicker'
 import { MuiSelect } from '~/components/GlobalStyles/GlobalStyles.mui'
 import Button from '~/components/Button'
 
 function ExecutionFilter({ options, selectedOption, disabled, onSelect, onDatePicker, onSearch }) {
+    const theme = useTheme()
+
     return (
         <Stack direction={'row'}>
             <FormControl sx={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Typography component={'span'}>Repository: </Typography>
+                <Typography fontSize={14} fontWeight={600} color={theme.palette.common.white} component={'span'}>
+                    Repository:
+                </Typography>
                 <MuiSelect sx={{ marginLeft: 1, width: 200 }} value={selectedOption} onChange={onSelect}>
                     <MenuItem value={''}>
                         <em>None</em>
@@ -26,11 +31,15 @@ function ExecutionFilter({ options, selectedOption, disabled, onSelect, onDatePi
                 </MuiSelect>
             </FormControl>
             <Stack marginLeft={1} spacing={1} direction={'row'} alignItems={'center'}>
-                <Typography component={'span'}>From date: </Typography>
+                <Typography fontSize={14} fontWeight={600} color={theme.palette.common.white} component={'span'}>
+                    From date:
+                </Typography>
                 <DateTimePicker sx={{ width: 150 }} onChange={(value) => onDatePicker('start', value.$d)} />
             </Stack>
             <Stack marginLeft={1} spacing={1} direction={'row'} alignItems={'center'}>
-                <Typography component={'span'}>To date: </Typography>
+                <Typography fontSize={14} fontWeight={600} color={theme.palette.common.white} component={'span'}>
+                    To date:
+                </Typography>
                 <DateTimePicker sx={{ width: 150 }} onChange={(value) => onDatePicker('end', value.$d)} />
             </Stack>
             <Button
