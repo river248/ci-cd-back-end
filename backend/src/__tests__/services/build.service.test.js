d7ae325959811729bc87393bcb90074c633bcbc3
Phan Xuan Giang <giang.phan@elca.vn>
2023-08-10 02:34:12 +0700

    test triggerBuildInQueue

--- backend/src/__tests__/services/build.service.test.js
+++ backend/src/__tests__/services/build.service.test.js
@@ -3,6 +3,55 @@ import { BuildService } from '~/services/buid.service'
 import { RepositoryService } from '~/services/repository.service'
 
 const EXEPECTED_ARG = 'ci-cd-github-actions'
+const TAG_NAME = 'master@0.0.2'
+const request = jest.fn().mockImplementation(
+    () =>
+        new Promise((resolve, _reject) => {
+            resolve({ status: 302, headers: { location: 'mock-url' } })
+        }),
+)
+global._octokit = { request }
+
+test('triggerBuildInQueue return null', async () => {
+    QueueModel.findQueue = jest.fn().mockImplementation(() => Promise.resolve([]))
+    QueueModel.removeFromQueue = jest.fn()
+    const res = await BuildService.triggerBuildInQueue(EXEPECTED_ARG)
+
+    expect(QueueModel.findQueue).toHaveBeenCalledWith(EXEPECTED_ARG)
+    expect(QueueModel.removeFromQueue).not.toHaveBeenCalled()
+    expect(global._octokit.request).not.toHaveBeenCalled()
+    expect(res).toBeNull()
+})
+
+test('triggerBuildInQueue return tagName', async () => {
+    QueueModel.findQueue = jest.fn().mockImplementation(() =>
+        Promise.resolve([
+            { repository: EXEPECTED_ARG, tagName: TAG_NAME, createdAt: 123456678123 },
+            { repository: EXEPECTED_ARG, tagName: 'master@0.0.3', createdAt: 123456678123 },
+        ]),
+    )
+    QueueModel.removeFromQueue = jest.fn()
+
+    const res = await BuildService.triggerBuildInQueue(EXEPECTED_ARG)
+
+    expect(QueueModel.findQueue).toHaveBeenCalledWith(EXEPECTED_ARG)
+    expect(QueueModel.removeFromQueue).toHaveBeenCalledWith(EXEPECTED_ARG, TAG_NAME)
+    expect(global._octokit.request).toHaveBeenCalledTimes(1)
+    expect(res).toBe(TAG_NAME)
+})
+
+test('triggerBuildInQueue throw error', async () => {
+    QueueModel.findQueue = jest
+        .fn()
+        .mockImplementation(() => Promise.reject(new Error('Server is temporator unvailable!')))
+
+    try {
+        await BuildService.triggerBuildInQueue(EXEPECTED_ARG)
+    } catch (error) {
+        expect(error.message).toBe('Server is temporator unvailable!')
+        expect(error.status).toBe(500)
+    }
+})
 
 test('getQueue return an empty array', async () => {
     QueueModel.findQueue = jest.fn().mockImplementation(() => Promise.resolve([]))
